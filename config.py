3333b91aa14fc0690342de083ee9158f3e322d88
Prabha569 <prabhakarnayak993@gmail.com>
2026-08-31 19:16:11 +0530

    ok

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Settings:
     # Format: postgresql://<user>:<password>@<host>:<port>/<database>
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "postgresql://axion_user:P%40ssw01rd%40123@localhost:5432/axion_db",
+        "postgresql://admin:P%40ssw01rd%40123@10.244.0.253:5432/axiondb",
     )
     # Allows configuring a different port, e.g., if we run multiple services
     PORT: int = int(os.getenv("PORT", "8000"))
